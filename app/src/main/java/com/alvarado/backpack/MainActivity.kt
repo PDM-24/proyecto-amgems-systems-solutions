7020cc81965d364ce65b33269e7f3652f97b080f
AxelAlvarado34 <axelalvarado34091@gmail.com>
2024-05-10 19:31:28 -0600

    Add : 💄 Login Screen

--- app/src/main/java/com/alvarado/backpack/MainActivity.kt
+++ app/src/main/java/com/alvarado/backpack/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.alvarado.backpack.ui.screens.LoginScreen
 import com.alvarado.backpack.ui.theme.BackPackTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             BackPackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BackPackTheme {
-        Greeting("Android")
-    }
-}
